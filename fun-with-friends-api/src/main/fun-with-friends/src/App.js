724d632cd37d1a567e4713631fe53075154f4100
Austin <apolanco115@gmail.com>
2019-11-13 15:31:19 -0500

    changed from browser router to hash router, mvn clean install in docker compose

---
+++ fun-with-friends-api/src/main/fun-with-friends/src/App.js
@@ -0,0 +1,27 @@
+import React, {Component} from "react";
+import {HashRouter as Router, Switch, Route} from 'react-router-dom';
+import DrawWithFriends from "./sketches/DrawWithFriends"
+import Login from "./components/Login";
+import CreateRoom from "./components/CreateRoom";
+import PongWithFriends from "./sketches/PongWithFriends";
+import Slider from "./components/Slider";
+
+class App extends Component {
+
+    render() {
+        return (
+            <Router>
+                <div>
+                    <Switch>
+                       <Route exact path='/' component={Login}/>
+                       <Route path='/draw-wf' component={DrawWithFriends}/>
+                    {/*    /!*<Route path='/pong-wf' component={PongWithFriends}/>*!/*/}
+                    {/*    <Route path='/rooms' component={CreateRoom}/>*/}
+                    </Switch>
+                </div>
+            </Router>
+        );
+    }
+}
+
+export default App;
---
+++ fun-with-friends-api/target/classes/public/precache-manifest.5a6084658341422d770ea3bef21d9993.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8351bee8f2fd495ef74fe43bc3fbb9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "652323c3271a280b41de",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "bc982a02f613e3a5cf5d",
+    "url": "/static/js/2.8469225a.chunk.js"
+  },
+  {
+    "revision": "652323c3271a280b41de",
+    "url": "/static/js/main.e4b03ce0.chunk.js"
+  },
+  {
+    "revision": "eab85eae591ea5895992",
+    "url": "/static/js/runtime-main.18c0ad7f.js"
+  }
+]);
