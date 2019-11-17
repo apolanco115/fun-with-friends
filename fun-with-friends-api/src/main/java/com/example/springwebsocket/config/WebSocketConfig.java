78448b72da8fa810dca20ac66921d48986b3c9e5
Austin <apolanco115@gmail.com>
2019-11-17 10:05:01 -0500

    commented websocketconfig

--- fun-with-friends-api/src/main/java/com/example/springwebsocket/config/WebSocketConfig.java
+++ fun-with-friends-api/src/main/java/com/example/springwebsocket/config/WebSocketConfig.java
@@ -9,12 +9,13 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
+    //websocket endpoints creation
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry){
         registry.addEndpoint("/play").withSockJS();
     }
 
+    //handles distribution of messages to connected parties
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry){
         registry.enableSimpleBroker("/topic");
